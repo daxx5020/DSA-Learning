4e59638c10c5babbcad5f83d6d9d5d4cda789b79
daxx5020 <dakshmakwana2146@gmail.com>
2025-06-25 01:12:10 +0530

    learning pascal triange

--- DSA/SDE-sheet/1-setMatrixZeroes.php
+++ DSA/SDE-sheet/1-setMatrixZeroes.php
@@ -44,27 +44,70 @@
 
 // Better approch
 
+// function setMatrixZeroes(&$matrix){
+//     $n = count($matrix);
+//     $m = count($matrix[0]);
+
+//     $row = array_fill(0,10,0);
+//     $col = array_fill(0,10,0);
+
+//     for ($i = 0; $i < $n; $i++){
+//         for ($j = 0; $j < $m; $j++){
+//             if ($matrix[$i][$j] == 0){
+//                 $row[$i] = 1;
+//                 $col[$j] = 1;
+//             }
+//         }
+//     }
+
+//     for ($i = 0; $i < $n; $i++){
+//         for ($j = 0; $j < $m; $j++){
+//             if($row[$i] == 1 || $col[$j] == 1){
+//                 $matrix[$i][$j] = 0;
+//             }
+//         }
+//     }
+
+//     return $matrix;
+// }
+
 function setMatrixZeroes(&$matrix){
     $n = count($matrix);
     $m = count($matrix[0]);
 
-    $row = array_fill(0,10,0);
-    $col = array_fill(0,10,0);
+    $col0 = 1;
 
     for ($i = 0; $i < $n; $i++){
         for ($j = 0; $j < $m; $j++){
             if ($matrix[$i][$j] == 0){
-                $row[$i] = 1;
-                $col[$j] = 1;
+                $matrix[$i][0] = 0;
+                if ($j != 0){
+                    $matrix[0][$j] = 0;
+                }
+                else{
+                    $col0 = 0;
+                }
             }
         }
     }
 
-    for ($i = 0; $i < $n; $i++){
-        for ($j = 0; $j < $m; $j++){
-            if($row[$i] == 1 || $col[$j] == 1){
+    for ($i = 1; $i < $n; $i++){
+        for ($j = 1; $j < $m; $j++){
+            if ($matrix[$i][0] == 0 || $matrix[0][$j] == 0){
                 $matrix[$i][$j] = 0;
             }
+        }
+    }
+
+    if ($matrix[0][0] == 0){
+        for ($j = 0; $j < $m; $j++){
+            $matrix[0][$j] = 0;
+        }
+    }
+
+    if ($col0 == 0){
+        for ($i = 0; $i < $n; $i++){
+            $matrix[$i][0] = 0;
         }
     }
 
--- DSA/SDE-sheet/2-pascalTriangle.php
+++ DSA/SDE-sheet/2-pascalTriangle.php
@@ -1,7 +1,63 @@
 <?php
 
 
-function pascalTriangle(){
+// Formula of ncr
+
+// function nCr($n,$r){
+//     $res = 1;
+
+//     for ($i = 0; $i < $r; $i++){
+//         $res = $res * ($n - $i);
+//         $res = $res / ($i + 1);
+//     }
+
+//     return (int)$res;
+// }
+
+// echo nCr(5, 2);
+
+
+// Print the full row of pascal triangel
+
+// function printRow($n){
+//     $ans = 1;
+//     echo $ans;
+//     for ($i = 1; $i < $n; $i++){
+//         $ans = $ans * ($n - $i);
+//         $ans = $ans / ($i);
+//         echo $ans;
+//     }
+// }
+
+// $n = 3;
+// printRow($n);
+
+
+
+// Print pscal triangle
+
+function generateRow($row){
+    $ans = 1;
+    $ansRow = [];
+    array_push($ansRow, $ans);
+
+    for ($i = 1; $i < $row; $i++){
+        $ans = $ans * ($row - $i);
+        $ans = $ans / ($i);
+        array_push($ansRow, $ans);
+    }
+
+    return $ansRow;
 
 }
 
+function pascalTriangle($n){
+    $ans = [];
+    for ($i = 1; $i <= $n; $i++){
+        array_push($ans, generateRow($i));
+    }
+    return $ans;
+}
+
+
+print_r(pascalTriangle(6));
