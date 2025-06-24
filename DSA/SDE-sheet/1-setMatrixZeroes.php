989bc48056033b5596a7f4df0a0ba13fbf3a49d9
daxx5020 <dakshmakwana2146@gmail.com>
2025-06-24 23:42:07 +0530

    learning set matrix zero better approch

--- DSA/SDE-sheet/1-setMatrixZeroes.php
+++ DSA/SDE-sheet/1-setMatrixZeroes.php
@@ -1,43 +1,76 @@
 <?php
 
+
+// Brutefoce approch
+
+// function setMatrixZeroes(&$matrix){
+//     $n = count($matrix);
+//     $m = count($matrix[0]);
+
+//     for ($i = 0; $i < $n; $i++) {
+//         for ($j = 0; $j < $m; $j++) {
+//             if ($matrix[$i][$j] === 0) {
+//                 $this->markRow($matrix, $i, $m);
+//                 $this->markCol($matrix, $j, $n);
+//             }
+//         }
+//     }
+
+//     // 🔧 Convert only our marker (null) to 0
+//     for ($i = 0; $i < $n; $i++) {
+//         for ($j = 0; $j < $m; $j++) {
+//             if ($matrix[$i][$j] === null) {
+//                 $matrix[$i][$j] = 0;
+//             }
+//         }
+//     }
+// }
+
+// function markRow(&$matrix, $i, $m){
+//     for ($j = 0; $j < $m; $j++){
+//         if ($matrix[$i][$j] !== 0 && $matrix[$i][$j] !== null){
+//             $matrix[$i][$j] = null;
+//         }
+//     }
+// }
+
+// function markCol(&$matrix, $j, $n){
+//     for ($i = 0; $i < $n; $i++){
+//         if ($matrix[$i][$j] !== 0 && $matrix[$i][$j] !== null){
+//             $matrix[$i][$j] = null;
+//         }
+//     }
+// }
+
+// Better approch
+
 function setMatrixZeroes(&$matrix){
     $n = count($matrix);
     $m = count($matrix[0]);
 
-    for ($i = 0; $i < $n; $i++) {
-        for ($j = 0; $j < $m; $j++) {
-            if ($matrix[$i][$j] === 0) {
-                $this->markRow($matrix, $i, $m);
-                $this->markCol($matrix, $j, $n);
+    $row = array_fill(0,10,0);
+    $col = array_fill(0,10,0);
+
+    for ($i = 0; $i < $n; $i++){
+        for ($j = 0; $j < $m; $j++){
+            if ($matrix[$i][$j] == 0){
+                $row[$i] = 1;
+                $col[$j] = 1;
             }
         }
     }
 
-    // 🔧 Convert only our marker (null) to 0
-    for ($i = 0; $i < $n; $i++) {
-        for ($j = 0; $j < $m; $j++) {
-            if ($matrix[$i][$j] === null) {
+    for ($i = 0; $i < $n; $i++){
+        for ($j = 0; $j < $m; $j++){
+            if($row[$i] == 1 || $col[$j] == 1){
                 $matrix[$i][$j] = 0;
             }
         }
     }
+
+    return $matrix;
 }
 
-function markRow(&$matrix, $i, $m){
-    for ($j = 0; $j < $m; $j++){
-        if ($matrix[$i][$j] !== 0 && $matrix[$i][$j] !== null){
-            $matrix[$i][$j] = null;
-        }
-    }
-}
-
-function markCol(&$matrix, $j, $n){
-    for ($i = 0; $i < $n; $i++){
-        if ($matrix[$i][$j] !== 0 && $matrix[$i][$j] !== null){
-            $matrix[$i][$j] = null;
-        }
-    }
-}
 
 
 $matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]];
